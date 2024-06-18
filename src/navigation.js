adc77af44b86e53d3f878cd82e22dac873dab351
S4nkry7 <107291299+S4nkry7@users.noreply.github.com>
2024-06-18 00:48:09 -0600

    Se sube footer

--- src/navigation.js
+++ src/navigation.js
@@ -141,29 +141,26 @@ export const headerData = {
 export const footerData = {
   links: [
     {
-      title: 'Product',
+      title: 'Colaboradores',
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
+        { text: 'Jurado Santos Alberto Isaac', href: '#' },
+        { text: 'Ponce Ortega Miguel Angel ', href: '#' },
+        { text: 'Estrada Corona Alexis ', href: '#' },
+        { text: 'Sánchez Ramos Jorge Alberto ', href: '#' },
+        { text: 'Velazquez Hernandez Aldo ', href: '#' },
       ],
     },
     {
-      title: 'Platform',
+      title: 'Nosotros',
       links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
+        { text: 'Acta Constitutiva', href: '/nosotros/actaConstitutiva' },
+        { text: 'Filosofia Organizacional', href: '/nosotros/filosofiaOrganizacional' },
+        { text: 'Estructura Organizacional', href: '/nosotros/estructuraOrganizacional' },
+        { text: 'Cultura Organizacional', href: '/nosotros/culturaOrganizacional' },
       ],
     },
     {
-      title: 'Support',
+      title: 'Desarrollo',
       links: [
         { text: 'Docs', href: '#' },
         { text: 'Community Forum', href: '#' },
@@ -185,19 +182,4 @@ export const footerData = {
       ],
     },
   ],
-  secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
-  ],
-  socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
-  ],
-  footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
-  `,
 };
